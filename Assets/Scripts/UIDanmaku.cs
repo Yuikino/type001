5814a57a9d67fa0043e011bbe357b3200dc27f42
Type <893093841@qq.com>
2020-02-23 02:45:16 +0800

    天才泰普的第二课

---
+++ Assets/Scripts/UIDanmaku.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class UIDanmaku : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Text text;
+    public float speed;
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            rectTransform.position = new Vector3(rectTransform.position.x - speed * Time.deltaTime, rectTransform.position.y, rectTransform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/VideoDanmaku.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoDanmaku : MonoBehaviour
+{
+    public Transform laneParent;
+    public UIDanmaku uiDanmakuPrefab;
+    public Button pauseButton;
+    public InputField danmakuInput;
+
+    private VideoPlayer videoPlayer;
+    // 为什么不用Queue？因为Queue不能插入到最前面，那么到时候发送弹幕的时候不好处理
+    private List<Danmaku> danmakuList = new List<Danmaku>();
+    private List<Transform> laneList = new List<Transform>();
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        for (int i = 0; i < laneParent.childCount; i++)
+        {
+            laneList.Add(laneParent.GetChild(i));
+        }
+        var loveYouForeverRest = new LoveYouForeverRest("http://seto.gicp.net");
+        loveYouForeverRest.ListDanmakus("welcome", result =>
+        {
+            if (result.status == "ok")
+            {
+                danmakuList.AddRange(result.list);
+                videoPlayer.Play();
+            }
+        }, (url, responseCode, error) =>
+        {
+            Debug.Log($"url: {url}, responseCode: {responseCode}, error: {error}");
+        });
+        pauseButton.onClick.AddListener(() =>
+        {
+            videoPlayer.Pause();
+        });
+        danmakuInput.onEndEdit.AddListener(text =>
+        {
+            AddDanmaku addDanmaku = new AddDanmaku();
+            addDanmaku.text = text;
+            addDanmaku.time = (float)videoPlayer.clockTime;
+            addDanmaku.scene = "welcome";
+            loveYouForeverRest.AddDanamku(addDanmaku, result =>
+            {
+                if (result.status == "ok")
+                {
+                    Danmaku danmaku = new Danmaku();
+                    danmaku.text = addDanmaku.text;
+                    danmaku.time = addDanmaku.time;
+                    danmakuList.Insert(0, danmaku);
+                }
+            }, null);
+        });
+    }
+
+
+    void Update()
+    {
+        // 获得当前视频时间
+        var time = videoPlayer.clockTime;
+        int toRemove = 0;
+        foreach (var danmaku in danmakuList)
+        {
+            // 如果弹幕的时间小于当前时间，代表要被显示
+            if (danmaku.time < time)
+            {
+                // 显示弹幕
+                ShowDanmaku(danmaku);
+                // 移除弹幕
+                toRemove++;
+            }
+            else// 如果弹幕时间大于等于当前时间，代表不用被显示，调用break中断循环
+            {
+                break;
+            }
+        }
+        // 遍历一下处理掉多少条弹幕就删掉多少条
+        for (int i = 0; i < toRemove; i++)
+        {
+            // 每次删掉第一条
+            danmakuList.RemoveAt(0);
+        }
+    }
+
+    void ShowDanmaku(Danmaku danmaku)
+    {
+        UIDanmaku uiDanmaku = Instantiate(uiDanmakuPrefab);
+        uiDanmaku.text.text = danmaku.text;
+        uiDanmaku.videoPlayer = videoPlayer;
+        uiDanmaku.transform.SetParent(laneList[Random.Range(0, laneList.Count - 1)]);
+        uiDanmaku.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+    }
+}
